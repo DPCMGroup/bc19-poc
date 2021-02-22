b231844fec4ca65e362490d485b42faad841b417
DanieleSpigolon <72398566+DanieleSpigolon@users.noreply.github.com>
2021-02-22 16:46:28 +0100

    Creati component e routing

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -3,16 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RoomComponent } from './room/room.component';
+import { ShowRoomComponent } from './Room/show-room/show-room.component';
+import { AddEditRoomComponent } from './Room/add-edit-room/add-edit-room.component';
+import {SharedService} from './shared.service';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RoomComponent,
+    ShowRoomComponent,
+    AddEditRoomComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [SharedService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
