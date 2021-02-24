b8225414e6a4260414251dc95e533e81712c3e85
samudg7 <samusasa7@gmail.com>
2021-02-24 21:39:41 +0100

    contratto stanza #1

--- Ethereum/StanzaPoc/private/truffle/migrations/2_deploy_contracts.js
+++ Ethereum/StanzaPoc/private/truffle/migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
-var SimpleStorage = artifacts.require("SimpleStorage");
+var Stanza = artifacts.require("SimpleStorage");
 
 module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
+  deployer.deploy(Stanza);
 };
