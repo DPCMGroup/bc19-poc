0aa6ba1b39f7c6e55dc6d446925f8b2f1d357ef6
antonio <antonio.badan@gmail.com>
2021-02-28 17:29:55 +0100

    aggiunto stato postazione, eliminati segnali

--- Ethereum/StanzaPoc/private/truffle/contracts/PostazioneMapping.sol
+++ Ethereum/StanzaPoc/private/truffle/contracts/PostazioneMapping.sol
@@ -4,13 +4,10 @@ pragma solidity ^0.7.4;
 
 contract SimpleStorage {
     
-    event CreazionePostazione(bool esito);
-    event EliminazionePostazione(bool esito);
-    
  struct Postazione {
      uint x;
      uint y;
-     bool isExist;
+     string stato;
  }
  
  //dati della stanza
@@ -18,24 +15,20 @@ contract SimpleStorage {
  uint dim2;
  mapping (string => Postazione) public postazioni; 
  
- // marcare mapping PUBLIC rende disponibile il getter aGGRATIS
- 
- //serve una lista degli id delle postazioni per eliminare tutte le postazioni della stanza
- //altrimenti si crea garbage
+// marcare mapping PUBLIC => getter implicito
+// se id non presente nel mapping, getter restituisce: (0,0,"")
  
  function _setDimensioniStanza(uint _dim1, uint _dim2) public {
      dim1=_dim1;
      dim2=_dim2;
  }
  
- function _creaPostazione (string memory _id, uint _x, uint _y) public {
-     postazioni[_id]=Postazione(_x, _y, true);
-     emit CreazionePostazione(postazioni[_id].isExist);
+ function _creaPostazione (string memory _id, uint _x, uint _y, string memory _stato) public {
+     postazioni[_id]=Postazione(_x, _y, _stato);
  }
  
  function _eliminaPostazione (string memory _id) public {
      delete postazioni[_id];
-     emit CreazionePostazione(postazioni[_id].isExist);
  }
     
 }
